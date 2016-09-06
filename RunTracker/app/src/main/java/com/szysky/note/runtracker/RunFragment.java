5a5de6dfc694f9f691af3565b32eb64513adf3ce
suzeyu <suzeyu_1992@163.com>
2016-09-06 11:50:43 +0800

    增添于LocationManager进行通讯的管理类

--- RunTracker/app/src/main/java/com/szysky/note/runtracker/RunFragment.java
+++ RunTracker/app/src/main/java/com/szysky/note/runtracker/RunFragment.java
@@ -49,7 +49,5 @@ public class RunFragment extends Fragment {
         return  rootView;
 
 
-
-
     }
 }
---
+++ RunTracker/app/src/main/java/com/szysky/note/runtracker/RunManager.java
@@ -0,0 +1,104 @@
+package com.szysky.note.runtracker;
+
+import android.Manifest;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.LocationManager;
+import android.support.v4.app.ActivityCompat;
+
+/**
+ * Author :  suzeyu
+ * Time   :  2016-09-06  上午11:07
+ * Blog   :  http://szysky.com
+ * GitHub :  https://github.com/suzeyu1992
+ *
+ * ClassDescription :   为管理与LocationManager的通讯, 所以创建一个单例管理类专门管理
+ */
+
+public class RunManager {
+
+    private static final String ACTION_LOCATION = "com.szysky.note.runtracker.ACTION_LOCATION";
+    private static volatile RunManager sRunManager;
+
+    /**
+     *  应用进程的上下文对象
+     */
+    private final Context mAppContext;
+    private final LocationManager mLocationManager;
+
+    private RunManager(Context context) {
+        mAppContext = context.getApplicationContext();
+        mLocationManager = (LocationManager) mAppContext.getSystemService(Context.LOCATION_SERVICE);
+    }
+
+    public static RunManager getInstance(Context context) {
+        if (sRunManager != null) {
+            synchronized (RunManager.class) {
+                if (sRunManager != null) {
+                    sRunManager = new RunManager(context);
+                }
+            }
+        }
+
+        return sRunManager;
+    }
+
+    /**
+     *  用于创建广播, 使用一个定制操作名来识别应用内的事件.
+     *  @param shouldCreate 标识参数, 告诉PendingIntent.getBroadcast()方法是否应该在系统内创建新的PendingIntent
+     */
+    private PendingIntent getLocationPendingIntent(boolean shouldCreate) {
+        Intent broadcast = new Intent(ACTION_LOCATION);
+        int flags = shouldCreate ? 0 : PendingIntent.FLAG_NO_CREATE;
+        return PendingIntent.getBroadcast(mAppContext, 0, broadcast, flags);
+    }
+
+    /**
+     *  方法功能: 开启地理位置
+     *  要求LocationManager通过GPS定位装置提供实时的定位数据更新.
+     *  其中的requestLocationUpdates()方法中参数2和3, 表示最小等待时间(毫秒)以及最短移动距离(米)
+     *  可用于发送下一次定位数据更新要移动的距离和要等待的时间.
+     */
+    public void startLocationUpdates() {
+        String gpsProvider = LocationManager.GPS_PROVIDER;
+
+        //  start updates from the location manager
+        PendingIntent pi = getLocationPendingIntent(true);
+        if (ActivityCompat.checkSelfPermission(
+                mAppContext, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+                && ActivityCompat.checkSelfPermission(mAppContext, Manifest.permission.ACCESS_COARSE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        mLocationManager.requestLocationUpdates(gpsProvider, 0, 0, pi);
+    }
+
+    public void stopLocationUpdates(){
+        PendingIntent pi = getLocationPendingIntent(false);
+
+        if (pi != null){
+            mLocationManager.removeUpdates(pi);
+            pi.cancel();
+        }
+    }
+
+    /**
+     *  通过getLocationPendingIntent()方法的返回值, 可以确定PendingIntent是否已在操作系统中登记
+     */
+    public boolean isTrackingRun(){
+        return getLocationPendingIntent(false)!= null;
+    }
+
+
+
+
+}
